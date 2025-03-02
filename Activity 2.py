a1b20a50a696e095478b55215bff8c308ab628cc
Avanthikacoding <aviedayilliam@gmail.com>
2025-03-02 12:25:57 +0000

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,6 @@
+def weather_condition_different():
+    print("The weather is wonderful for wearing no coats in " , spring)
+    print("The weather is the same in " , autumn , " but you need to wear a jumper")
+spring = "autumn"
+autumn = spring
+weather_condition_different()
